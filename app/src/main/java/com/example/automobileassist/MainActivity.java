c87f297b5611100ccb130e78d87d17e4f88d3d9e
sudil <sudilhasithaa51@gmail.com>
2019-08-03 12:09:11 +0530

    Main menu of the app

--- app/src/main/java/com/example/automobileassist/MainActivity.java
+++ app/src/main/java/com/example/automobileassist/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.automobileassist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button garageBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        garageBtn = (Button)findViewById(R.id.buttonGarage);
+
+        garageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,GarageRegistrationActivity.class));
+            }
+        });
+
     }
 }
+
+
---
+++ app/src/main/java/com/example/automobileassist/SubMenu.java
@@ -0,0 +1,18 @@
+package com.example.automobileassist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class SubMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_menu);
+
+    }
+
+}
